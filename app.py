94f2cfdad318993e9e434f91f998e0ca3ca58bd3
Murphj99 <john.murphy99@mail.dcu.ie>
2020-07-03 11:18:51 +0000

    failing on heroku removed folder

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello ():
     return 'Hello World.....again'
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get('IP'),
-        port=(os.environ.get('PORT')),
+    app.run(host=os.environ.get('0.0.0.0'),
+        port=(os.environ.get('5000')),
         debug=True)
